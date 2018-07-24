fda3204af968083ce6087b016b48af90a033df92
Thomas Steur <tsteur@users.noreply.github.com>
2018-07-25 07:37:47 +1200

    Support assigning object values in Custom HTML tag (#39)

--- tests/Integration/ReleaseCheckListTest.php
+++ tests/Integration/ReleaseCheckListTest.php
@@ -29,7 +29,10 @@ class ReleaseCheckListTest extends IntegrationTestCase
 
         shell_exec("cat ". $pathTagManagerJs ." | java -jar ". PIWIK_DOCUMENT_ROOT ."/tests/resources/yuicompressor/yuicompressor-2.4.8.jar --type js --line-break 1000 | sed 's/replacedEvilString/eval/' | sed 's/^[/][*]/\/*!/' > " . $testMinifiedJs);
 
-        $this->assertFileEquals($testMinifiedJs,
+        $testMinifiedJs = trim(file_get_contents($testMinifiedJs));
+        $pathTagManagerJsMin = trim(file_get_contents($pathTagManagerJsMin));
+
+        $this->assertSame($testMinifiedJs,
             $pathTagManagerJsMin,
             'minified tagmanager.min.js is out of date, please re-generate the minified files using instructions in tagmanager.js'
         );
