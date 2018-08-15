489900a579e12e924760670bdbf67d04dae03144
Lukas Winkler <git@lw1.at>
2018-08-15 13:12:25 +0200

    add In-Page Apps

--- Template/Tag/ShareaholicTag.php
+++ Template/Tag/ShareaholicTag.php
@@ -31,6 +31,30 @@ class ShareaholicTag extends BaseTag
                 $field->description = 'Create a new site in Shareaholic and paste the site ID here';
                 $field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
                 $field->validators[] = new NotEmpty();
+            }),
+            $this->makeSetting('shareaholicInPageApp', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
+                $field->title = 'Shareaholic In-Page App';
+                $field->uiControl = FieldConfig::UI_CONTROL_SINGLE_SELECT;
+                $field->availableValues = array(
+                    '' => 'None',
+                    'share_buttons' => 'Share Buttons',
+                    'follow_buttons' => 'Follow Buttons',
+                    'recommendations' => 'Related Content',
+                    'total_share_count'=> 'Total Share Counter' //TODO: Also supports data-link and data-services
+                );
+
+                $field->description = 'Here you can optionally add an In-Page App to your website.';
+            }),
+
+            $this->makeSetting('shareaholicAppId', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
+                $field->title = 'In-Page App ID';
+                $field->description = 'If you want to add an In-Page App enter the App ID here. It is about eight digits long. The App ID is not required for the Total Share Counter.';
+                $field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
+            }),
+            $this->makeSetting('shareaholicParentSelector', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
+                $field->title = 'In-Page App Parent Selector';
+                $field->description = 'Enter a CSS selector to the element where the In-Page App should be added.';
+                $field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
             })
         );
     }
--- Template/Tag/ShareaholicTag.web.js
+++ Template/Tag/ShareaholicTag.web.js
@@ -14,6 +14,28 @@
                 script.setAttribute('data-shr-siteid', siteId);
                 script.src = '//apps.shareaholic.com/assets/pub/shareaholic.js';
                 s.parentNode.insertBefore(script, s);
+                console.info(s);
+                var inPageApp = parameters.get('shareaholicInPageApp');
+                if (inPageApp) {
+                    var AppID = parameters.get('shareaholicAppId');
+                    var parentSelector = parameters.get('shareaholicParentSelector');
+                    if (inPageApp !== 'total_share_count' && !AppID) {
+                        TagManager.debug.error('this In-Page App requires an AppID');
+                        return false;
+                    }
+                    if (!parentSelector) {
+                        TagManager.debug.error('you have to specify a Parent Selector to place the In-Page App onto your website');
+                        return false;
+                    }
+                    var parent = TagManager.dom.bySelector(parentSelector)[0];
+                    var div = document.createElement('div');
+                    div.className = 'shareaholic-canvas';
+                    div.setAttribute('data-app', inPageApp);
+                    if (AppID) {
+                        div.setAttribute('data-app-id', AppID);
+                    }
+                    parent.appendChild(div)
+                }
             }
         };
     };
