951ef5ff64519bd1605fb3d508bacb1aeff2f1e6
Lukas Winkler <git@lw1.at>
2018-08-17 10:18:20 +0200

    better validation

--- Template/Tag/ShareaholicTag.php
+++ Template/Tag/ShareaholicTag.php
@@ -25,6 +25,19 @@ class ShareaholicTag extends BaseTag
 
     public function getParameters()
     {
+        $InPageApp = $this->makeSetting('shareaholicInPageApp', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
+            $field->title = 'Shareaholic In-Page App';
+            $field->uiControl = FieldConfig::UI_CONTROL_SINGLE_SELECT;
+            $field->availableValues = array(
+                '' => 'None',
+                'share_buttons' => 'Share Buttons',
+                'follow_buttons' => 'Follow Buttons',
+                'recommendations' => 'Related Content',
+                'total_share_count' => 'Total Share Counter' //TODO: Also supports data-link and data-services
+            );
+
+            $field->description = 'Here you can optionally add an In-Page App to your website.';
+        });
         return array(
             $this->makeSetting('shareaholicSiteId', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
                 $field->title = 'Shareaholic Site ID';
@@ -32,31 +45,32 @@ class ShareaholicTag extends BaseTag
                 $field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
                 $field->validators[] = new NotEmpty();
             }),
-            $this->makeSetting('shareaholicInPageApp', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
-                $field->title = 'Shareaholic In-Page App';
-                $field->uiControl = FieldConfig::UI_CONTROL_SINGLE_SELECT;
-                $field->availableValues = array(
-                    '' => 'None',
-                    'share_buttons' => 'Share Buttons',
-                    'follow_buttons' => 'Follow Buttons',
-                    'recommendations' => 'Related Content',
-                    'total_share_count'=> 'Total Share Counter' //TODO: Also supports data-link and data-services
-                );
-
-                $field->description = 'Here you can optionally add an In-Page App to your website.';
-            }),
-
-            $this->makeSetting('shareaholicAppId', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
+            $InPageApp,
+            $this->makeSetting('shareaholicAppId', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) use ($InPageApp) {
                 $field->title = 'In-Page App ID';
                 $field->description = 'If you want to add an In-Page App enter the App ID here. It is about eight digits long. The App ID is not required for the Total Share Counter.';
                 $field->condition = 'shareaholicInPageApp && shareaholicInPageApp !== "total_share_count"';
                 $field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
+                $field->validate = function ($value) use ($InPageApp, $field) {
+                    if (!empty($InPageApp->getValue()) && $InPageApp->getValue() != 'total_share_count' && empty($value)) {
+                        throw new \Exception('You need to specify an App ID if you want to use a InPageApp');
+                    }
+                    if ($InPageApp->getValue() == 'total_share_count' && !empty($value)) {
+                        throw new \Exception("The Total Share Counter doesn't need an App ID");
+                    }
+                };
+
             }),
-            $this->makeSetting('shareaholicParentSelector', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
+            $this->makeSetting('shareaholicParentSelector', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) use ($InPageApp) {
                 $field->title = 'In-Page App Parent Selector';
                 $field->description = 'Enter a CSS selector to the element where the In-Page App should be added.';
                 $field->condition = 'shareaholicInPageApp';
                 $field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
+                $field->validate = function ($value) use ($InPageApp, $field) {
+                    if (!empty($InPageApp->getValue()) && empty($value)) {
+                        throw new \Exception('You need to specify a Parent Selector if you want to use a InPageApp');
+                    }
+                };
             })
         );
     }
