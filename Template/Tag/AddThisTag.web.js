3a73ee92075b2ef15b3675eedc332c34f728ef26
Lukas Winkler <git@lw1.at>
2018-08-25 11:44:36 +0200

    add inline div before adding Javascript

--- Template/Tag/AddThisTag.web.js
+++ Template/Tag/AddThisTag.web.js
@@ -1,6 +1,16 @@
 (function () {
     return function (parameters, TagManager) {
         this.fire = function () {
+            var parentSelector = parameters.get('AddThisParentSelector');
+            if (parentSelector) {
+                var parent = TagManager.dom.bySelector(parentSelector);
+                if (parent) {
+                    var div = document.createElement('div');
+                    div.className = 'addthis_inline_share_toolbox';
+                    parent[0].appendChild(div)
+                }
+            }
+
             var siteId = parameters.get('AddThisPubId');
             if (siteId) {
                 var script = document.createElement('script');
@@ -8,16 +18,6 @@
                 script.type = 'text/javascript';
                 script.src = '//s7.addthis.com/js/300/addthis_widget.js#pubid=' + siteId;
                 s.parentNode.insertBefore(script, s);
-
-                var parentSelector = parameters.get('AddThisParentSelector');
-                if (parentSelector) {
-                    var parent = TagManager.dom.bySelector(parentSelector);
-                    if (parent) {
-                        var div = document.createElement('div');
-                        div.className = 'addthis_inline_share_toolbox';
-                        parent[0].appendChild(div)
-                    }
-                }
             }
         };
     };
