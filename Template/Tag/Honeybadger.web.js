088e139e0cc14769aca64f940cb4d0e46d88a968
Thomas Steur <tsteur@users.noreply.github.com>
2018-08-27 07:34:07 +1200

    fix minor typo

--- Template/Tag/Honeybadger.web.js
+++ Template/Tag/Honeybadger.web.js
@@ -6,11 +6,11 @@
                 var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
                 s1.src = '//js.honeybadger.io/v0.5/honeybadger.min.js';
                 s1.setAttribute('data-apiKey', apiKey);
-                var environmet = parameters.get('honeybadgerEnvironment');
-                if (!environmet) {
-                    environmet = TagManager.Container.environment
+                var environment = parameters.get('honeybadgerEnvironment');
+                if (!environment) {
+                    environment = TagManager.Container.environment
                 }
-                s1.setAttribute('data-environment', environmet);
+                s1.setAttribute('data-environment', environment);
                 var revision = parameters.get('honeybadgerRevision');
                 if (revision) {
                     s1.setAttribute('data-revision', revision)
