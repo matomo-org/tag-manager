67004d22aef32e4506def95e6523e234c6a3dca7
ben-dw <43407231+ben-dw@users.noreply.github.com>
2018-10-04 11:42:26 +0200

    Delete EtrackerEventTag.web.js

--- Template/Tag/EtrackerEventTag.web.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    return function (parameters, TagManager) {
-        this.fire = function () {
-				_etracker.sendEvent(new et_UserDefinedEvent(parameters.get('etrackerEventObject'), parameters.get('etrackerEventCategory'), parameters.get('etrackerEventAction'), parameters.get('etrackerEventType')));
-            };
-        };
-    }
-)();
