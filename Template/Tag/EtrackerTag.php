e719bde6f071e839a7286ee4fb527f94ce35d180
ben-dw <43407231+ben-dw@users.noreply.github.com>
2018-10-08 15:12:53 +0200

    Update EtrackerTag.php
    Merged etracker tag and etracker event tag.

--- Template/Tag/EtrackerTag.php
+++ Template/Tag/EtrackerTag.php
@@ -19,16 +19,54 @@ class EtrackerTag extends BaseTag
     
     public function getParameters()
     {
-        return array(
-            $this->makeSetting('etrackerID', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
-                $field->title = 'etracker Tracking ID';
-                $field->description = '';
-                $field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
-                $field->validators[] = new NotEmpty();
+         $trackingType = $this->makeSetting('trackingType', 'pageview', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
+            $field->title = 'Tracking Type';
+            $field->description = 'Choose which action should be executed when this tag is fired.';
+            $field->uiControl = FieldConfig::UI_CONTROL_SINGLE_SELECT;
+            $field->validators[] = new NotEmpty();
+            $field->availableValues = array(
+                'pageview' => 'Pageview',
+                'event' => 'Event',
+            );
+        });
+	    return array(
+		$trackingType,
+            	$this->makeSetting('etrackerID', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) use ($trackingType) {
+                	$field->title = 'etracker Tracking ID';
+                	$field->description = '';
+                	$field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
+			$field->condition = 'trackingType == "pageview"';
+                	$field->validators[] = new NotEmpty();
             }),
-			$this->makeSetting('etrackerDNT', false, FieldConfig::TYPE_BOOL, function (FieldConfig $field) {
-                $field->title = 'etracker Do-Not-Track';
-                $field->description = 'Respect Do Not Track';
+		$this->makeSetting('etrackerDNT', false, FieldConfig::TYPE_BOOL, function (FieldConfig $field) use ($trackingType) {
+                	$field->title = 'etracker Do-Not-Track';
+                	$field->description = 'Respect Do Not Track';
+			$field->condition = 'trackingType == "pageview"';
+            }),
+		$this->makeSetting('etrackerEventCategory', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) use ($trackingType) {
+                	$field->title = 'etracker Category';
+                	$field->description = 'The event\'s category, for example Navigation, Outbound Links, 404 Error...';
+                	$field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
+			$field->condition = 'trackingType == "event"';
+                	$field->validators[] = new NotEmpty();
+            }),
+		$this->makeSetting('etrackerEventObject', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) use ($trackingType) {
+                	$field->title = 'etracker Object';
+                	$field->description = 'The event\'s object Name, for example a particular navigation element, a clicked element, form name,...';
+                	$field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
+			$field->condition = 'trackingType == "event"';
+            }),
+		$this->makeSetting('etrackerEventAction', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) use ($trackingType) {
+                	$field->title = 'etracker Action';
+                	$field->description = 'The event\'s action, for example, click, open, close, play, pause...';
+                	$field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
+			$field->condition = 'trackingType == "event"';
+            }),
+		$this->makeSetting('etrackerEventType', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) use ($trackingType) {
+                	$field->title = 'etracker Type';
+                	$field->description = 'The event\'s type Name, for example an value of a send form...';
+                	$field->customUiControlTemplateFile = self::FIELD_TEMPLATE_VARIABLE;
+			$field->condition = 'trackingType == "event"';
             })
         );
     }
