c70464cab4d4b25e135e6121a0f357e383bf7409
ben-dw <43407231+ben-dw@users.noreply.github.com>
2018-10-08 14:57:45 +0200

    Update EtrackerEventTag.php

--- Template/Tag/EtrackerEventTag.php
+++ Template/Tag/EtrackerEventTag.php
@@ -14,7 +14,7 @@ class EtrackerEventTag extends BaseTag
 {
 	public function getIcon()
 	{
-		return 'plugins/TagManager/Template/Tag/EtrackerEvent.svg';
+		return 'plugins/TagManager/Template/Tag/Etracker.svg';
 	}
     public function isCustomTemplate()
     {
