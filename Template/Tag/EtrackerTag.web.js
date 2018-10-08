fa60992bba2a20a5c79a1943f7018e2fba83e6ab
ben-dw <43407231+ben-dw@users.noreply.github.com>
2018-10-08 15:26:57 +0200

    Update EtrackerTag.web.js
    Merged etracker tag and etracker event tag to one web.js file.

--- Template/Tag/EtrackerTag.web.js
+++ Template/Tag/EtrackerTag.web.js
@@ -1,22 +1,27 @@
 (function () {
     return function (parameters, TagManager) {
-        this.fire = function () {			
+        this.fire = function () {
+		var trackingType = parameters.get('trackingType');
+		if (trackingType === 'pageview') {
 			var etrackerID = parameters.get('etrackerID');
-            if (etrackerID) {
-                var script = document.createElement('script');
-                var s = document.getElementsByTagName('script')[0];
-                script.type = 'text/javascript';
-                script.setAttribute('data-secure-code', etrackerID);
+            		if (etrackerID) {
+				var script = document.createElement('script');
+				var s = document.getElementsByTagName('script')[0];
+				script.type = 'text/javascript';
+				script.setAttribute('data-secure-code', etrackerID);
 				script.setAttribute('id', '_etLoader');
 				script.setAttribute('charset', 'UTF-8');
 				var etrackerDNT = parameters.get('etrackerDNT');
 				if(etrackerDNT){
 					script.setAttribute('data-respect-dnt', 'true');
-				}
-                script.src = '//static.etracker.com/code/e.js';
-                s.parentNode.insertBefore(script, s);               
-            }				
-            };
+					}
+				script.src = '//static.etracker.com/code/e.js';
+				s.parentNode.insertBefore(script, s);               
+			}
+		} else if (trackingType === 'event') {
+			_etracker.sendEvent(new et_UserDefinedEvent(parameters.get('etrackerEventObject'), parameters.get('etrackerEventCategory'), parameters.get('etrackerEventAction'), parameters.get('etrackerEventType')));
+		}
+		};
         };
     }
 )();
