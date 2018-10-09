2673739b641a800063747993c34a534d7fbf78ca
Lukas Winkler <git@lw1.at>
2018-10-09 10:00:16 +0200

    detect selected text

--- Template/Trigger/CopyTrigger.web.js
+++ Template/Trigger/CopyTrigger.web.js
@@ -7,12 +7,23 @@
                 }
                 var target = event.target;
                 var dom = TagManager.dom;
+                var selectedText;
+                if (window.getSelection) {
+                    selectedText = window.getSelection().toString();
+                } else if (document.selection && document.selection.type !== 'Control') {
+                    selectedText = document.selection.createRange().text;
+                } else {
+                    TagManager.debug.error("can't detect selected text");
+                    return false;
+                }
+                console.info(selectedText);
                 triggerEvent({
                     event: 'mtm.Copy',
                     'mtm.copyElement': target,
                     'mtm.copyElementId': dom.getElementAttribute(target, 'id'),
                     'mtm.copyElementName': dom.getElementAttribute(target, 'name'),
-                    'mtm.copyElementClasses': dom.getElementClassNames(target)
+                    'mtm.copyElementClasses': dom.getElementClassNames(target),
+                    'mtm.copyTextLength': selectedText.length
                 });
             });
         };
