073b93806fd16ae5b413c72267844fcadb33b923
ben-dw <43407231+ben-dw@users.noreply.github.com>
2018-11-14 09:00:16 +0100

    Update EtrackerTag.web.js
    Fire only once the script.src per pageview.

--- Template/Tag/EtrackerTag.web.js
+++ Template/Tag/EtrackerTag.web.js
@@ -3,31 +3,36 @@
         this.fire = function () {
         var etrackerConfig = parameters.get('etrackerConfig', {});
         var trackingType = parameters.get('trackingType');
-        if (trackingType === 'pageview') {
-            var etrackerID = etrackerConfig.etrackerID;
-                if (etrackerID) {
-                // Pageview parameters for overwrite
-                for (var i in etrackerConfig) {
-                    if (TagManager.utils.hasProperty(etrackerConfig, i)) {
-                        if(i.startsWith('et_')){
-                            window[i] = etrackerConfig[i];
-                        }
+        var etrackerID = etrackerConfig.etrackerID;
+        
+		if (trackingType === 'pageview') {
+            // Pageview parameters for overwrite
+            for (var i in etrackerConfig) {
+                if (TagManager.utils.hasProperty(etrackerConfig, i)) {
+                    if(i.startsWith('et_')){
+                        window[i] = etrackerConfig[i];
                     }
                 }
-                // Pageview script
-                var s = document.getElementsByTagName('script')[0];
-                var script = document.createElement('script');
-                script.type = 'text/javascript';
-                script.setAttribute('data-secure-code', etrackerID);
-                script.setAttribute('id', '_etLoader');
-                script.setAttribute('charset', 'UTF-8');
-                if (etrackerConfig.etrackerDNT) {
-                    script.setAttribute('data-respect-dnt', 'true');
-                }
-                script.src = '//static.etracker.com/code/e.js';
-                s.parentNode.insertBefore(script, s);               
             }
-        } else if (trackingType === 'event') {
+        }
+
+        if (!hasBeenLoaded && etrackerID) {
+            hasBeenLoaded = true;
+            // Pageview script
+            var s = document.getElementsByTagName('script')[0];
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.setAttribute('data-secure-code', etrackerID);
+            script.setAttribute('id', '_etLoader');
+            script.setAttribute('charset', 'UTF-8');
+            if (etrackerConfig.etrackerDNT) {
+                script.setAttribute('data-respect-dnt', 'true');
+            }
+            script.src = '//static.etracker.com/code/e.js';
+            s.parentNode.insertBefore(script, s);
+         }
+        
+        if (trackingType === 'event') {
             _etracker.sendEvent(new et_UserDefinedEvent(parameters.get('etrackerEventObject'), parameters.get('etrackerEventCategory'), parameters.get('etrackerEventAction'), parameters.get('etrackerEventType')));
         }
         };
