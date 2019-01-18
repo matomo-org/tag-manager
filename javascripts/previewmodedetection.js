26c48296e4b6f85b50e69a1fea9c5b4f8fd5f56f
Thomas Steur <tsteur@users.noreply.github.com>
2019-01-18 20:32:24 +1300

    add random id to preview url to avoid browser cache

--- javascripts/previewmodedetection.js
+++ javascripts/previewmodedetection.js
@@ -1,6 +1,6 @@
 if ((document.cookie.indexOf('$cookieId') !== -1 && window.location.search.indexOf('&$urlParamDisableId') === -1 && window.location.search.indexOf('?$urlParamDisableId') === -1) || window.location.search.indexOf('&$urlParamEnabledId') !== -1 || window.location.search.indexOf('?$urlParamEnabledId') !== -1) {
-
+    var rand = Math.floor(Math.random() * 999999998) + 1;
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=false; g.defer=false; g.src='$previewUrl'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=false; g.defer=false; g.src='$previewUrl?rand=' + rand; s.parentNode.insertBefore(g,s);
     return;
 }
