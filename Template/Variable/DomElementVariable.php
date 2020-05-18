ab51723a9015b90c7d8cab6e8febb96596cea1d8
Lukas Winkler <git@lw1.at>
2020-05-18 21:41:53 +0200

    better CSS Selector help (#244)

--- Template/Variable/DomElementVariable.php
+++ Template/Variable/DomElementVariable.php
@@ -33,7 +33,7 @@ class DomElementVariable extends BaseVariable
             $selectionMethod,
             $this->makeSetting('cssSelector', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) use ($selectionMethod) {
                 $field->title = 'CSS Selector';
-                $field->description = 'A CSS selector allows you to select an element by id, className, element names, etc. If multiple elements match this selector, the first matching element will be used to get the value from.';
+                $field->description = 'A CSS selector allows you to select an element by id, className, element names, etc. If multiple elements match this selector, the first matching element will be used to get the value from. Examples for valid selectors are ".classname", "#id" or "li a".';
                 $field->condition = 'selectionMethod == "cssSelector"';
                 $field->validate = function ($value) use ($selectionMethod, $field) {
                     if ($selectionMethod->getValue() === 'cssSelector' && empty($value)) {
