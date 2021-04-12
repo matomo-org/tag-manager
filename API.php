0af1365d0cd2cdf1aff327724f3d8f8bc0da0927
Zoltan Flamis <flamisz@gmail.com>
2021-04-12 16:57:30 +1200

    decode variable name on update

--- API.php
+++ API.php
@@ -881,6 +881,7 @@ class API extends \Piwik\Plugin\API
 
         $parameters = $this->unsanitizeAssocArray($parameters);
         $lookupTable = $this->unsanitizeAssocArray($lookupTable);
+        $name = urldecode($name);
 
         $return = $this->variables->updateContainerVariable($idSite, $idContainerVersion, $idVariable, $name, $parameters, $defaultValue, $lookupTable);
 
