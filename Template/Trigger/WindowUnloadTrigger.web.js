bd95a4950302aba4d762afc3465852f525f420dd
Thomas Steur <tsteur@users.noreply.github.com>
2021-08-31 08:10:38 +1200

    Ensure window unload is triggered only once
    fix https://github.com/matomo-org/tag-manager/issues/349

--- Template/Trigger/WindowUnloadTrigger.web.js
+++ Template/Trigger/WindowUnloadTrigger.web.js
@@ -1,7 +1,12 @@
 (function () {
     return function (parameters, TagManager) {
         this.setUp = function (triggerEvent) {
+            var triggered = false;
             TagManager.dom.addEventListener(parameters.window, 'beforeunload', function () {
+                if (triggered) {
+                    return;
+                }
+                triggered = true;
                 triggerEvent({event: 'WindowUnload'});
             });
         };
