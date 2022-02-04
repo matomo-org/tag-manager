dce5fc4e61d8f89aba004c82115bfaec077c2063
diosmosis <diosmosis@users.noreply.github.com>
2022-02-04 08:59:21 -0800

    removed too many JS files for some reason

--- TagManager.php
+++ TagManager.php
@@ -551,11 +551,41 @@ class TagManager extends \Piwik\Plugin
 
         $jsFiles[] = "plugins/TagManager/javascripts/tagmanagerHelper.js";
 
+        $jsFiles[] = "plugins/TagManager/angularjs/manageContainer/model.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageContainer/list.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageContainer/list.directive.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageContainer/edit.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageContainer/edit.directive.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageContainer/manage.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageContainer/manage.directive.js";
 
+        $jsFiles[] = "plugins/TagManager/angularjs/manageVersion/model.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageVersion/diff.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageVersion/list.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageVersion/list.directive.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageVersion/edit.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageVersion/edit.directive.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageVersion/manage.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageVersion/manage.directive.js";
 
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTrigger/model.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTrigger/list.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTrigger/list.directive.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTrigger/edit.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTrigger/edit.directive.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTrigger/manage.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTrigger/manage.directive.js";
 
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTag/model.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTag/list.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTag/list.directive.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTag/edit.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTag/edit.directive.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTag/manage.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageTag/manage.directive.js";
 
-
+        $jsFiles[] = "plugins/TagManager/angularjs/manageInstallCode/manage-install-tag-code.controller.js";
+        $jsFiles[] = "plugins/TagManager/angularjs/manageInstallCode/manage-install-tag-code.directive.js";
 
         $jsFiles[] = "plugins/TagManager/angularjs/containerDashboard/container-dashboard.controller.js";
         $jsFiles[] = "plugins/TagManager/angularjs/containerDashboard/container-dashboard.directive.js";
