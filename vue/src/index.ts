f145a806d887b2ef792f52502b05530f23814b83
dizzy <diosmosis@users.noreply.github.com>
2022-02-08 10:46:33 -0800

    [Vue] migrate select-variable-type directive to vue component (#407)
    * migrate import version directive to vue component
    
    * remove angularjs files
    
    * migrate select variable directive
    
    * fix notification tyoe
    
    * rebuild
    
    * add back some code
    
    * rebuild
    
    * remove angularjs files
    
    * migrate select-variable-type to VariableSelectType component
    
    * fix typing
    
    * fix function call and rebuild
    
    * fix event bind
    
    * rebuild
    
    * variable property should be modelValue so v-model can be used
    
    * rebuild
    
    * Update vue/src/VariableSelect/VariableSelect.vue
    
    Co-authored-by: Stefan Giehl <stefan@matomo.org>
    
    * apply review feedback
    
    * fix two-way binding in adapter
    
    Co-authored-by: Stefan Giehl <stefan@matomo.org>

--- vue/src/index.ts
+++ vue/src/index.ts
@@ -7,6 +7,8 @@
 
 import './ImportVersion/ImportVersion.adapter';
 import './VariableSelect/VariableSelect.adapter';
+import './VariableSelectType/VariableSelectType.adapter';
 
 export { default as ImportVersion } from './ImportVersion/ImportVersion.vue';
 export { default as VariableSelect } from './VariableSelect/VariableSelect.vue';
+export { default as VariableSelectType } from './VariableSelectType/VariableSelectType.vue';
