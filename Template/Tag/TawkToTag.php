9e137be60ada3896d3cf8501aab310bbf8e73210
Altamash Shaikh <altu9594@gmail.com>
2022-02-16 10:39:57 +0530

    Added changes for tawktotag to work with new widgetID changes, #426

--- Template/Tag/TawkToTag.php
+++ Template/Tag/TawkToTag.php
@@ -35,6 +35,12 @@ class TawkToTag extends BaseTag
                 $field->validators[] = new NotEmpty();
                 $field->validators[] = new CharacterLength(16, 30); // we limit to 30 so users don't accidentally enter a 32 digit API key
             }),
+            $this->makeSetting('tawkToWidgetId', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
+                $field->title = 'tawk.to Widget ID';
+                $field->description = 'You can get the Widget ID by logging into Tawk.to, going to "Administration" and clicking on "Chat Widget" and selecting an appropriate widget. The widget ID can be retrieved from "Direct Chat Link" as https://tawk.to/chat/{SITE_ID}/{WIDGET_ID}.';
+                $field->validators[] = new NotEmpty();
+                $field->validators[] = new CharacterLength(9, 20); // we limit to 20 so users don't accidentally enter a 32 digit API key
+            }),
         );
     }
 
--- Template/Tag/TawkToTag.web.js
+++ Template/Tag/TawkToTag.web.js
@@ -1,10 +1,12 @@
 (function () {
     return function (parameters, TagManager) {
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
         this.fire = function () {
             var tawkToId = parameters.get("tawkToId");
+            var tawkToWidgetId = parameters.get("tawkToWidgetId");
             var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
             s1.async = true;
-            s1.src = 'https://embed.tawk.to/' + tawkToId + '/default';
+            s1.src = 'https://embed.tawk.to/' + tawkToId + '/' + tawkToWidgetId;
             s1.charset = 'UTF-8';
             s1.setAttribute('crossorigin', '*');
             s0.parentNode.insertBefore(s1, s0);
