c7837f477c2389c820fd85d1e3c9ffc45ee8115a
diosmosis <diosmosis@users.noreply.github.com>
2022-02-21 20:03:12 -0800

    apply review feedback

--- vue/src/Field/FieldVariableTemplate.vue
+++ vue/src/Field/FieldVariableTemplate.vue
@@ -24,6 +24,7 @@
       :title="translate('TagManager_ChooseVariable')"
       @click="selectVariable()"
     ></span>
+    <br/>
     <label :for="name" v-html="$sanitize(title)"></label>
   </div>
 </template>
