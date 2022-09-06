71346a71ca3884aa4be3a7a6f1dbd4f0bdb381ff
diosmosis <diosmosis@users.noreply.github.com>
2022-09-06 05:06:00 -0700

    fix broken merge

--- Model/Variable.php
+++ Model/Variable.php
@@ -276,9 +276,8 @@ class Variable extends BaseModel
         $parameters = $entity['parameters'];
         foreach ($entity['typeMetadata']['parameters'] as $parameter) {
             $paramName = $parameter['name'];
-            if (($parameter['templateFile'] === BaseTemplate::FIELD_TEMPLATE_VARIABLE
-                    || (isset($parameter['component'])
-                        && in_array($parameter['component'], [BaseTemplate::FIELD_VARIABLE_COMPONENT, BaseTemplate::FIELD_VARIABLE_TYPE_COMPONENT])))
+            if ((isset($parameter['component'])
+                        && in_array($parameter['component'], [BaseTemplate::FIELD_VARIABLE_COMPONENT, BaseTemplate::FIELD_VARIABLE_TYPE_COMPONENT]))
                 && isset($parameters[$paramName])
                 && is_string($parameters[$paramName])
                 && strpos($parameters[$paramName], $oldVarNameTemplate) !== false) {
