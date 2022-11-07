65a424176d82642455415ba9526fb72abbc941f9
Tomas Persson <hypotesfabriken@gmail.com>
2022-11-07 22:30:07 +0100

    Changes to doUpdate
    Changes to doUpdate after comment from @snake14

--- Updates/4.12.4-b1.php
+++ Updates/4.12.4-b1.php
@@ -68,8 +68,8 @@ class Updates_4_12_4_b1 extends PiwikUpdates
     public function doUpdate(Updater $updater)
     {
         // Migrate the Matomo type tags to all include the newly configured field.
-        $migrator = new NewTagParameterMigrator(MatomoTag::ID, 'customCookieTimeOutEnable',0);
+        $migrator = new NewVariableParameterMigrator(MatomoConfigurationVariable::ID, 'customCookieTimeOutEnable',0);
         $migrator->addField('customCookieTimeOut',393); // This is optional and only needed if you added more than one parameter.
         $migrator->migrate(); // This kicks off the processing of the tag migration.
-    }
+    }    
 }
