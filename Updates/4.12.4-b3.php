2a61afd00064d73f9653bb2ce98b427b2328643a
Tomas Persson <hypotesfabriken@gmail.com>
2022-11-19 10:22:16 +0100

    Fixing tests and adding db updates
    Fixing tests to match new translations and also adding a new db update script

---
+++ Updates/4.12.4-b3.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Plugins\TagManager;
+
+use Piwik\Plugins\TagManager\Template\Variable\MatomoConfigurationVariable;
+use Piwik\Plugins\TagManager\Updates\NewVariableParameterMigrator;
+use Piwik\Updater;
+use Piwik\Updates as PiwikUpdates;
+
+/**
+ * Update for version 4.12.4-b1.
+ */
+class Updates_4_12_4_b3 extends PiwikUpdates
+{
+    public function doUpdate(Updater $updater)
+    {
+        $migrator2 = new NewTagParameterMigrator(MatomoTag::ID, 'searchKeyword');
+        $migrator2->addField('searchCategory','');
+        $migrator2->addField('searchCount','');
+        $migrator2->migrate();
+    }
+}
