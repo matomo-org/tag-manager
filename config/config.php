5da4173925b302002b6cb6f56bb67f04df2cd998
sgiehl <stefan@matomo.org>
2023-04-05 16:06:45 +0200

    use new vendor proxies

--- config/config.php
+++ config/config.php
@@ -15,16 +15,16 @@ return array(
         return 'container_';
     },
     'TagManagerJSMinificationEnabled' => true,
-    'fileintegrity.ignore' => DI\add(array(
-        DI\get('fileintegrityIgnoreTagManager')
+    'fileintegrity.ignore' => Piwik\DI::add(array(
+        Piwik\DI::get('fileintegrityIgnoreTagManager')
     )),
     'fileintegrityIgnoreTagManager' => function (\Psr\Container\ContainerInterface $c) {
         $start = trim($c->get('TagManagerContainerStorageDir'), '/');
         return $start . '/' . $c->get('TagManagerContainerFilesPrefix') . '*.js';
     },
-    'diagnostics.required' => DI\add(array(
-        DI\get('Piwik\Plugins\TagManager\Diagnostic\ContainerWriteAccess'),
+    'diagnostics.required' => Piwik\DI::add(array(
+        Piwik\DI::get('Piwik\Plugins\TagManager\Diagnostic\ContainerWriteAccess'),
     )),
-    'Piwik\Plugins\TagManager\Model\Container\ContainerIdGenerator' => DI\autowire('Piwik\Plugins\TagManager\Model\Container\RandomContainerIdGenerator'),
-    'Piwik\Plugins\TagManager\Context\Storage\StorageInterface' => DI\create('Piwik\Plugins\TagManager\Context\Storage\Filesystem'),
+    'Piwik\Plugins\TagManager\Model\Container\ContainerIdGenerator' => Piwik\DI::autowire('Piwik\Plugins\TagManager\Model\Container\RandomContainerIdGenerator'),
+    'Piwik\Plugins\TagManager\Context\Storage\StorageInterface' => Piwik\DI::create('Piwik\Plugins\TagManager\Context\Storage\Filesystem'),
 );
