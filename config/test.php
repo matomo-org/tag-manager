7f94cb6769b2d758bb1c558014030290507569e6
sgiehl <stefan@matomo.org>
2023-05-02 12:49:34 +0200

    fix test config

--- config/test.php
+++ config/test.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'Piwik\Plugins\TagManager\Context\Storage\StorageInterface' => Piwik\DI::autowire('Piwik\Plugins\TagManager\Context\Storage\Memory'),
-    'Piwik\Plugins\TagManager\Model\Container\ContainerIdGenerator' => Piwik\DI::decorate(function ($previous) {
+    'Piwik\Plugins\TagManager\Context\Storage\StorageInterface' => \Piwik\DI::autowire('Piwik\Plugins\TagManager\Context\Storage\Memory'),
+    'Piwik\Plugins\TagManager\Model\Container\ContainerIdGenerator' => \Piwik\DI::decorate(function ($previous) {
 
         $testGenerateFixedId = \Piwik\Container\StaticContainer::get('test.vars.testGenerateFixedId');
         if (!empty($testGenerateFixedId)) {
@@ -11,8 +11,8 @@ return array(
 
         return $previous;
     }),
-    'observers.global' => Piwik\DI::add(array(
-        array('AssetManager.getStylesheetFiles', Piwik\DI::value(function (&$stylesheets) {
+    'observers.global' => \Piwik\DI::add(array(
+        array('AssetManager.getStylesheetFiles', \Piwik\DI::value(function (&$stylesheets) {
             $useOverrideCss = \Piwik\Container\StaticContainer::get('test.vars.useOverrideCss');
             if ($useOverrideCss) {
                 $stylesheets[] = 'plugins/TagManager/tests/resources/uitest-override.css';
