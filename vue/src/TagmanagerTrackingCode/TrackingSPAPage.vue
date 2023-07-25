72b931058c1cc012e44faf744358a99637cfe33e
Michal Kleiner <mk@011.nz>
2023-07-26 00:51:12 +1200

    Backport translation of no-data screen for React and SPA/PWA to 4.x (#664)
    * Backport SPA/PWA translations from 5.x-dev
    
    * built vue files
    
    ---------
    
    Co-authored-by: sgiehl <stefan@matomo.org>

--- vue/src/TagmanagerTrackingCode/TrackingSPAPage.vue
+++ vue/src/TagmanagerTrackingCode/TrackingSPAPage.vue
@@ -16,19 +16,25 @@
     />
     <li v-html="$sanitize(setupStep2)"></li>
     <li v-html="$sanitize(fetchFollowStep3)"></li>
-    <li v-text="translate('TagManager_SPAFollowStep4')"></li>
-    <li v-html="$sanitize(fetchFollowStepCommon(5))"></li>
+    <li v-text="fetchClickX('TagManager_CreateNewTrigger')"></li>
+    <li v-html="$sanitize(fetchFollowStep5)"></li>
     <!-- Since both the buttons has same text we are using the step4's text here   -->
-    <li v-text="translate('TagManager_SPAFollowStep4')"></li>
+    <li v-text="fetchClickX('TagManager_CreateNewTrigger')"></li>
     <li v-html="$sanitize(setupStep7)"></li>
     <li v-html="$sanitize(fetchFollowStep8)"></li>
-    <li v-html="$sanitize(fetchFollowStepCommon(9))"></li>
-    <li v-html="$sanitize(fetchFollowStepCommon(10))"></li>
-    <li v-html="$sanitize(fetchFollowStepCommon(11))"></li>
-    <li v-text="translate('TagManager_SPAFollowStep12')"></li>
-    <li v-text="translate('TagManager_SPAFollowStep13')"></li>
-    <li v-html="$sanitize(fetchFollowStepCommon(14))"></li>
-    <li v-text="translate('TagManager_SPAFollowStep15')"></li>
+    <li v-html="$sanitize(fetchFollowStep9)"></li>
+    <li>
+      <span v-text="fetchFollowStep10"></span>
+      <ol style="list-style: lower-alpha; list-style-position: inside; text-indent: 1.2rem;">
+        <li v-html="$sanitize(fetchFollowStep10a)"></li>
+        <li v-html="$sanitize(fetchFollowStep10b)"></li>
+      </ol>
+    </li>
+    <li v-html="$sanitize(fetchFollowStep11)"></li>
+    <li v-text="fetchClickX('General_Update')"></li>
+    <li v-text="fetchFollowStep13"></li>
+    <li v-html="$sanitize(fetchFollowStep14)"></li>
+    <li v-text="fetchFollowStep15"></li>
     <li v-if="jsFramework === 'react'" v-html="$sanitize(fetchFollowStep16React)"></li>
     <li v-else v-html="$sanitize(fetchFollowStep16SPA)"></li>
     <div
@@ -149,11 +155,10 @@ export default defineComponent({
       }
       return `?${url}`;
     },
-    fetchFollowStepCommon(stepNo :number) {
+    fetchClickX(clickTarget: string) {
       return translate(
-        `TagManager_SPAFollowStep${stepNo}`,
-        '<strong>',
-        '</strong>',
+        'General_ClickX',
+        translate(clickTarget),
       );
     },
   },
@@ -161,19 +166,87 @@ export default defineComponent({
     fetchFollowStep3() {
       return translate(
         'TagManager_SPAFollowStep3',
-        '<strong>',
-        '</strong>',
+        `<strong>${translate('TagManager_PageViewTriggerName')}</strong>`,
+        translate('TagManager_PageViewTriggerName'),
         '<a href="https://matomo.org/faq/tag-manager/how-do-i-track-pageviews-of-my-website-using-matomo-tag-manager/#create-pageview-trigger" target="_blank" rel="noreferrer noopener">',
         '</a>',
+      );
+    },
+    fetchFollowStep5() {
+      return translate(
+        'TagManager_SPAFollowStep5',
+        `<strong>${translate('TagManager_HistoryChangeTriggerName')}</strong>`,
+        translate('TagManager_CategoryUserEngagement'),
       );
     },
     fetchFollowStep8() {
       return translate(
         'TagManager_SPAFollowStep8',
-        '<strong>',
-        '</strong>',
+        `<strong>${translate('TagManager_PageViewTriggerName')}</strong>`,
+        `<strong>${translate('TagManager_MatomoTagName')}</strong>`,
+        translate('TagManager_PageViewTriggerName'),
         '<a href="https://matomo.org/faq/tag-manager/how-do-i-track-pageviews-of-my-website-using-matomo-tag-manager/#create-pageview-tag" target="_blank" rel="noreferrer noopener">',
         '</a>',
+      );
+    },
+    fetchFollowStep9() {
+      return translate(
+        'TagManager_SPAFollowStep9',
+        translate('TagManager_ConfigureWhatTagDoes'),
+        translate('TagManager_CustomTitle'),
+        '<strong>',
+        '</strong>',
+      );
+    },
+    fetchFollowStep10() {
+      return translate(
+        'TagManager_SPAFollowStep10',
+        translate('TagManager_CustomUrl'),
+      );
+    },
+    fetchFollowStep10a() {
+      return translate(
+        'TagManager_SPAFollowStep10a',
+        '<strong>',
+        '</strong>',
+        translate('TagManager_CustomUrl'),
+      );
+    },
+    fetchFollowStep10b() {
+      return translate(
+        'TagManager_SPAFollowStep10b',
+        translate('TagManager_CustomUrl'),
+        '<strong>',
+        '</strong>',
+      );
+    },
+    fetchFollowStep11() {
+      return translate(
+        'TagManager_SPAFollowStep11',
+        translate('TagManager_ConfigureWhenTagDoes'),
+        translate('TagManager_FireTriggerTitle'),
+        `<strong>${translate('TagManager_HistoryChangeTriggerName')}</strong>`,
+        `<strong>${translate('TagManager_PageViewTriggerName')}</strong>`,
+      );
+    },
+    fetchFollowStep13() {
+      return translate(
+        'TagManager_SPAFollowStep13',
+        translate('TagManager_Publish'),
+      );
+    },
+    fetchFollowStep14() {
+      return translate(
+        'TagManager_SPAFollowStep14',
+        translate('TagManager_VersionName'),
+        '<strong>',
+        '</strong>',
+      );
+    },
+    fetchFollowStep15() {
+      return translate(
+        'TagManager_SPAFollowStep15',
+        translate('TagManager_CreateVersionAndPublishRelease'),
       );
     },
     fetchFollowStep16SPA() {
