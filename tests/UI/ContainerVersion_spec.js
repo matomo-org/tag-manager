c412d34f94b01b58a08a4aed397fea3376b2a0d9
sgiehl <stefan@matomo.org>
2023-08-16 17:49:19 +0200

    updates expected UI test files

--- tests/UI/ContainerVersion_spec.js
+++ tests/UI/ContainerVersion_spec.js
@@ -219,6 +219,7 @@ describe("ContainerVersion", function () {
 
     it('should be possible to confirm publish version to different environment', async function () {
         await modal.clickButton(page, 'Publish');
+        await page.mouse.move(-10, -10);
         await capture.page(page, 'publish_environment_confirmed');
     });
 
