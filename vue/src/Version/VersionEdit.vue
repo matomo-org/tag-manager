0e9a9c6d7001b387873000768ae9c0347d851611
Altamash Shaikh <altu9594@gmail.com>
2024-01-12 08:43:19 +0530

    formatting changes

--- vue/src/Version/VersionEdit.vue
+++ vue/src/Version/VersionEdit.vue
@@ -85,31 +85,31 @@
             <h3>{{ translate('TagManager_ChangesSinceLastVersion') }}:</h3>
             <table v-content-table>
               <thead>
-              <tr>
-                <th>{{ translate('SitesManager_Type') }}</th>
-                <th>{{ translate('General_Name') }}</th>
-                <th>{{ translate('TagManager_Change') }}</th>
-                <th>{{ translate('TagManager_LastUpdated') }}</th>
-              </tr>
+                <tr>
+                  <th>{{ translate('SitesManager_Type') }}</th>
+                  <th>{{ translate('General_Name') }}</th>
+                  <th>{{ translate('TagManager_Change') }}</th>
+                  <th>{{ translate('TagManager_LastUpdated') }}</th>
+                </tr>
               </thead>
               <tbody>
-              <tr v-if="isLoadingVersionChanges">
-                <td colspan="4">
-                  <ActivityIndicator
-                    :loading-message="translate('TagManager_DetectingChanges')"
-                    :loading="true"
-                  />
-                </td>
-              </tr>
-              <tr v-if="!versionChanges.length && !isLoadingVersionChanges">
-                <td colspan="4">{{ translate('UserCountryMap_None') }}</td>
-              </tr>
-              <tr v-for="(versionChange, index) in versionChanges" :key="index">
-                <td>{{ translate(versionChange.entityType) }}</td>
-                <td>{{ versionChange.name }}</td>
-                <td>{{ translate(versionChange.type) }}</td>
-                <td class="lastUpdated"><span>{{ versionChange.lastChanged }}</span></td>
-              </tr>
+                <tr v-if="isLoadingVersionChanges">
+                  <td colspan="4">
+                    <ActivityIndicator
+                      :loading-message="translate('TagManager_DetectingChanges')"
+                      :loading="true"
+                    />
+                  </td>
+                </tr>
+                <tr v-if="!versionChanges.length && !isLoadingVersionChanges">
+                  <td colspan="4">{{ translate('UserCountryMap_None') }}</td>
+                </tr>
+                <tr v-for="(versionChange, index) in versionChanges" :key="index">
+                  <td>{{ translate(versionChange.entityType) }}</td>
+                  <td>{{ versionChange.name }}</td>
+                  <td>{{ translate(versionChange.type) }}</td>
+                  <td class="lastUpdated"><span>{{ versionChange.lastChanged }}</span></td>
+                </tr>
               </tbody>
             </table>
           </div>
