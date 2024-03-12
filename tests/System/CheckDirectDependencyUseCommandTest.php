93f7f297ddd3d91fedb8d0a1018a93b3933dddf9
Altamash Shaikh <altu9594@gmail.com>
2024-03-13 01:33:00 +0530

    Added tests to check direct use of core dependencies, #PG-3334

---
+++ tests/System/CheckDirectDependencyUseCommandTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\TagManager\tests\System;
+
+use Piwik\Plugins\TestRunner\Commands\CheckDirectDependencyUse;
+use Piwik\Tests\Framework\TestCase\SystemTestCase;
+use Piwik\Version;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+
+class CheckDirectDependencyUseCommandTest extends SystemTestCase
+{
+    public function testCommand()
+    {
+        if (version_compare(Version::VERSION, '5.0.3', '<=') && !file_exists(PIWIK_INCLUDE_PATH . '/plugins/TestRunner/Commands/CheckDirectDependencyUse.php')) {
+            $this->markTestSkipped('tests:check-direct-dependency-use is not available in this version');
+        }
+
+        $pluginName = 'TagManager';
+
+        $checkDirectDependencyUse = new CheckDirectDependencyUse();
+
+        $console = new \Piwik\Console(self::$fixture->piwikEnvironment);
+        $console->addCommands([$checkDirectDependencyUse]);
+        $command = $console->find('tests:check-direct-dependency-use');
+        $arguments = [
+            'command'    => 'tests:check-direct-dependency-use',
+            '--plugin' => $pluginName,
+            '--grep-vendor',
+        ];
+
+        $inputObject = new ArrayInput($arguments);
+        $command->run($inputObject, new NullOutput());
+
+        $this->assertEquals([
+            'Symfony\Component\Console' => [
+                'TagManager/tests/System/CheckDirectDependencyUseCommandTest.php'
+            ],
+            'DI' => [
+                'TagManager/TagManager.php',
+            ]
+        ], $checkDirectDependencyUse->usesFoundList[$pluginName]);
+    }
+}
