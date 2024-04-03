7788380090ab6bf0d281ad30067b174455afa210
caddoo <matthew.caddoo@gmail.com>
2024-04-03 17:55:26 +1300

    rename requireConsentForCampaignTracking to disableCampaignParameters

--- Updates/5.1.0-b1.php
+++ Updates/5.1.0-b1.php
@@ -59,7 +59,7 @@ class Updates_5_1_0_b1 extends PiwikUpdates
         $updater->executeMigrations(__FILE__, $this->getMigrations($updater));
 
         // Migrate the MatomoConfiguration type variables to all include the newly configured fields.
-        $migrator = new NewVariableParameterMigrator(MatomoConfigurationVariable::ID, 'requireConsentForCampaignTracking', false);
+        $migrator = new NewVariableParameterMigrator(MatomoConfigurationVariable::ID, 'disableCampaignParameters', false);
         $migrator->migrate();
     }
 }
