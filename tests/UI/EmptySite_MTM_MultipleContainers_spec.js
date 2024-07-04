925c73459d6b11f88814603fcc025c534e653848
Jacob Ransom <jacob@innocraft.com>
2024-07-04 12:23:03 +1200

    Improve test case coverage a bit more

--- tests/UI/EmptySite_MTM_MultipleContainers_spec.js
+++ tests/UI/EmptySite_MTM_MultipleContainers_spec.js
@@ -7,20 +7,16 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
-describe("EmptySite_MTM_SingleSiteSingleContainer", function () {
+describe("EmptySite_MTM_MultipleContainers", function () {
 
-  this.fixture = "Piwik\\Tests\\Fixtures\\EmptySite";
+  this.fixture = "Piwik\\Plugins\\TagManager\\tests\\Fixtures\\TagManagerNoTrackingFixture";
 
-  const generalParams = 'idSite=1&period=day&date=2010-01-03';
+  const generalParams = 'idSite=2&period=day&date=2010-01-03';
 
-  it('should show the MTM tracking code if the website has no recorded data one container', async function () {
-
-    testEnvironment.detectedContentDetections = [];
-    testEnvironment.connectedConsentManagers = [];
-    testEnvironment.save();
-
+  it('should show the MTM tracking code if the website has no recorded data multiple containers', async function () {
     const urlToTest = "?" + generalParams + "&module=CoreHome&action=index#?" + generalParams + '&activeTab=matomotagmanager';
     await page.goto(urlToTest);
+    await page.waitForNetworkIdle();
     await page.waitForSelector('#start-tracking-details .codeblock', {visible: true});
     await page.evaluate(function () {
       // since containerID will be random and keeps changing
@@ -32,4 +28,11 @@ describe("EmptySite_MTM_SingleSiteSingleContainer", function () {
     expect(await pageElement.screenshot()).to.matchImage('emptySiteDashboard');
   });
 
+  it('should show the advanced options when clicked', async function () {
+    await page.click('div.advance-option > span > a');
+    await page.waitForSelector('#tagManagerTrackingCodeSite', {visible: true});
+
+    const pageElement = await page.$('.page');
+    expect(await pageElement.screenshot()).to.matchImage('showAdvanced');
+  });
 });
