9e385503762d6b567cfca3ee2dadd58778b2b2fc
araichyk <1289711+araichyk@users.noreply.github.com>
2024-07-21 23:51:26 -0400

    Fix <br> tag

--- Template/Tag/MatomoTag.php
+++ Template/Tag/MatomoTag.php
@@ -53,7 +53,7 @@ class MatomoTag extends BaseTag
         $isEcommerceView = $this->makeSetting('isEcommerceView', false, FieldConfig::TYPE_BOOL, function (FieldConfig $field) {
             $field->title = Piwik::translate('TagManager_MatomoTagEcommerceViewIsEcommerceView');
             $inlineHelpLine1 = StaticContainer::get('TagManager.MatomoTagEcommerceViewIsEcommerceViewHelpOnPremise');
-            $field->inlineHelp = $inlineHelpLine1 ? Piwik::translate($inlineHelpLine1) . '<br><br>' : '';
+            $field->inlineHelp = $inlineHelpLine1 ? Piwik::translate($inlineHelpLine1) . '<br /><br />' : '';
             $field->inlineHelp .= Piwik::translate('TagManager_MatomoTagEcommerceViewIsEcommerceViewHelpV2');
             $field->condition = 'trackingType == "pageview"';
         });
