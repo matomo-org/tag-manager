d62746e77dbc043690ded885c848ff5f5758ccf5
Jacob Ransom <jacob@innocraft.com>
2024-08-28 14:31:10 +1200

    Adjusting JS to work correctly

--- Template/Tag/LinkedinInsightTag.web.js
+++ Template/Tag/LinkedinInsightTag.web.js
@@ -7,14 +7,16 @@
                 window._linkedin_partner_id = partnerId;
                 window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
                 window._linkedin_data_partner_ids.push(window._linkedin_partner_id);
-                if (conversionId) {
-                    window._linkedin_data_conversion_id = conversionId;
-                }
                 (function(){
                     var s = document.getElementsByTagName("script")[0];
                     var b = document.createElement("script");
                     b.type = "text/javascript";b.async = true;
                     b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                    if (conversionId) {
+                        b.onload = function() {
+                            window.lintrk('track', {conversion_id: conversionId});
+                        }
+                    }
                     s.parentNode.insertBefore(b, s);
                 })();
             }
