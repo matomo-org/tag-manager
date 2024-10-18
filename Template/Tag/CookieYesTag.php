8024e0fafdc5e5c43d46ca0bb8ae3f89ff1eba90
Altamash Shaikh <altu9594@gmail.com>
2024-10-18 11:41:48 +0530

    Added Axeptio, CookieYes and OneTrust consent manager tags, #PG-3597

---
+++ Template/Tag/CookieYesTag.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+namespace Piwik\Plugins\TagManager\Template\Tag;
+
+use Piwik\Piwik;
+use Piwik\Settings\FieldConfig;
+use Piwik\Validators\NotEmpty;
+
+class CookieYesTag extends BaseTag
+{
+    public function getName()
+    {
+        // By default, the name will be automatically fetched from the TagManager_CustomHtmlTagName translation key.
+        // you can either adjust/create/remove this translation key, or return a different value here directly.
+        return parent::getName();
+    }
+
+    public function getDescription()
+    {
+        // By default, the description will be automatically fetched from the TagManager_CustomHtmlTagDescription
+        // translation key. you can either adjust/create/remove this translation key, or return a different value
+        // here directly.
+        return Piwik::translate('TagManager_CookieYesTagDescription');
+    }
+
+    public function getHelp()
+    {
+        // By default, the help will be automatically fetched from the TagManager_CustomHtmlTagHelp translation key.
+        // you can either adjust/create/remove this translation key, or return a different value here directly.
+        return parent::getHelp();
+    }
+
+    public function getCategory()
+    {
+        return Piwik::translate('TagManager_ConsentManagementPlatform');
+    }
+
+    public function getIcon()
+    {
+        // You may optionally specify a path to an image icon URL, for example:
+        //
+        // return 'plugins/TagManager/images/MyIcon.png';
+        //
+        // to not return default icon call:
+        // return parent::getIcon();
+        //
+        // The image should have ideally a resolution of about 64x64 pixels.
+        return 'plugins/TagManager/images/icons/cookieyes.svg';
+    }
+
+    public function getParameters()
+    {
+        return array(
+            $this->makeSetting('cookiebotWebsiteKey', '', FieldConfig::TYPE_STRING, function (FieldConfig $field) {
+                $field->title = Piwik::translate('TagManager_CookieYesTagWebsiteKeyTitle');
+                $field->description = Piwik::translate('TagManager_CookieYesTagWebsiteKeyDescription');
+                $field->customFieldComponent = self::FIELD_VARIABLE_COMPONENT;
+                $field->validators[] = new NotEmpty();
+            }),
+        );
+    }
+
+}
