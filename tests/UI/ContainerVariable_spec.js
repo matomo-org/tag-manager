7b1f48817ffa6ebdfe73733bc2bfcb0c15074056
Altamash Shaikh <altu9594@gmail.com>
2025-05-23 15:19:48 +0530

    Fixes for UI test

--- tests/UI/ContainerVariable_spec.js
+++ tests/UI/ContainerVariable_spec.js
@@ -162,6 +162,8 @@ describe("ContainerVariable", function () {
         await setVariableName('CustomJSVariable after write user edit');
         await capture.page(page, 'custom_js_variable_write_user_not_editable_after_edit');
         await cancelVariable();
+        await page.reload();
+        await page.waitForNetworkIdle();
         await clickFirstRowTableAction('icon-delete', 3);
         await modal.clickButton(page, 'Yes');
         await page.waitForNetworkIdle();
