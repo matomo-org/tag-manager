ce9b6687b8c7efaa391f72b9c27df3a05b425b4c
Jacob Ransom <jacob@innocraft.com>
2026-09-23 11:35:32 +1200

    Complete TagManager license headers [ignore_release]

--- config/tracker.php
+++ config/tracker.php
@@ -1,3 +1,10 @@
 <?php
 
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
 return array();
